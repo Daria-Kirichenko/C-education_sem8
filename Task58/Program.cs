425160d86ca3fb0fdf96dfa592545f1ddc5b8da0
Daria <darakirichenko@gmail.com>
2023-03-13 21:09:32 +0300

    Task58 is done

---
+++ Task58/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задайте две матрицы. 
+// Напишите программу, которая будет находить произведение двух матриц.
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetRandomMatrix(int rows, int columns, int leftBorder, int rightBorder)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Random.Shared.Next(leftBorder, rightBorder);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = ReadNumber("Введите количество строк:");
+int n = ReadNumber("Введите количество столбцов:");
+
+int[,] firstMatrix = GetRandomMatrix(m, n, 0, 10);
+int[,] secondMatrix = GetRandomMatrix(m, n, 0, 10);
+PrintMatrix(firstMatrix);
+Console.WriteLine();
+PrintMatrix(secondMatrix);
+Console.WriteLine();
+
+int[,] GetMatrixMultiplying(int rows, int columns, int leftBorder, int rightBorder)
+{
+    int[,] matrix = new int[rows, columns];
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = firstMatrix[i, j] * secondMatrix[i, j];
+        }
+    }
+    return matrix;
+}
+
+
+int[,] resultMatrix = GetMatrixMultiplying(m, n, 0, 10);
+PrintMatrix(resultMatrix);
